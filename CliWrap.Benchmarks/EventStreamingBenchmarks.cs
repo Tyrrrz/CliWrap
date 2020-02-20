3ff5d7873875387cc619d254a4071ceda905d71a
Alexey Golub <tyrrrz@gmail.com>
2020-02-20 22:49:48 +0200

    Event streaming bench

---
+++ CliWrap.Benchmarks/EventStreamingBenchmarks.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using CliWrap.EventStream;
+using Cysharp.Diagnostics;
+
+namespace CliWrap.Benchmarks
+{
+    [MemoryDiagnoser, Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class EventStreamingBenchmarks
+    {
+        private const string FilePath = "dotnet";
+        private static readonly string Args = $"{Tests.Dummy.Program.Location} {Tests.Dummy.Program.LoopBoth} 100000";
+
+        [Benchmark(Description = "CliWrap", Baseline = true)]
+        public async Task<int> ExecuteWithCliWrap()
+        {
+            var counter = 0;
+            await foreach (var cmdEvent in Cli.Wrap(FilePath).WithArguments(Args).ListenAsync())
+            {
+                cmdEvent
+                    .OnStandardOutput(_ => counter++)
+                    .OnStandardError(_ => counter++);
+            }
+
+            return counter;
+        }
+
+        [Benchmark(Description = "ProcessX")]
+        public async Task<int> ExecuteWithProcessX()
+        {
+            var counter = 0;
+
+            var (_, stdOutStream, stdErrStream) = ProcessX.GetDualAsyncEnumerable(FilePath, arguments: Args);
+
+            var consumeStdOutTask = Task.Run(async () =>
+            {
+                await foreach (var _ in stdOutStream)
+                {
+                    counter++;
+                }
+            });
+
+            var consumeStdErrorTask = Task.Run(async () =>
+            {
+                await foreach (var _ in stdErrStream)
+                {
+                    counter++;
+                }
+            });
+
+            await Task.WhenAll(consumeStdOutTask, consumeStdErrorTask);
+
+            return counter;
+        }
+    }
+}
