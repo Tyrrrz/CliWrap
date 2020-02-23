af1ec506780c351bd1118385571cc7f09ca5e6bf
Alexey Golub <tyrrrz@gmail.com>
2020-02-24 01:40:23 +0200

    Cleanup

--- CliWrap.Tests/EnvironmentSpecs.cs
+++ CliWrap.Tests/EnvironmentSpecs.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CliWrap.Buffered;
-using CliWrap.Tests.Internal;
 using FluentAssertions;
 using Xunit;
 
--- CliWrap/EventStream/Channel.cs
+++ CliWrap/EventStream/Channel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
