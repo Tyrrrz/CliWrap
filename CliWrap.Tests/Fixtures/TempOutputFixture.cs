e066b2db49476b1aa85d78233f9a760b22ad8669
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2021-11-22 19:22:06 +0200

    Refactor temp output in tests

--- CliWrap.Tests/Fixtures/TempOutputFixture.cs
+++ CliWrap.Tests/Fixtures/TempOutputFixture.cs
@@ -1,30 +1,51 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace CliWrap.Tests.Fixtures
 {
     public class TempOutputFixture : IDisposable
     {
-        public string DirPath { get; } = Path.Combine(
-            Path.GetDirectoryName(typeof(TempOutputFixture).Assembly.Location) ?? Directory.GetCurrentDirectory(),
-            "Temp"
-        );
+        private readonly string _dirPath =
+            Path.GetDirectoryName(typeof(TempOutputFixture).Assembly.Location) ??
+            Directory.GetCurrentDirectory();
 
-        public TempOutputFixture() => Directory.CreateDirectory(DirPath);
+        private readonly ConcurrentBag<string> _filePaths = new();
 
-        public string GetTempFilePath(string fileName) => Path.Combine(DirPath, fileName);
+        public string GetTempFilePath(string fileName)
+        {
+            var filePath = Path.Combine(_dirPath, fileName);
+            _filePaths.Add(filePath);
 
-        public string GetTempFilePath() => GetTempFilePath(Guid.NewGuid().ToString());
+            return filePath;
+        }
+
+        public string GetTempFilePath() => GetTempFilePath($"Test-{Guid.NewGuid()}.tmp");
 
         public void Dispose()
         {
-            try
+            var exceptions = new List<Exception>();
+
+            foreach (var filePath in _filePaths)
             {
-                Directory.Delete(DirPath, true);
+                try
+                {
+                    File.Delete(filePath);
+                }
+                catch (FileNotFoundException)
+                {
+                    // Ignore
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
             }
-            catch (DirectoryNotFoundException)
-            {
-            }
+
+            if (exceptions.Any())
+                throw new AggregateException(exceptions);
         }
     }
 }
