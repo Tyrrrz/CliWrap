e83f78a48e2bb2240be0ffcf4b3277ef5c3d1a44
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2022-01-04 04:53:50 +0330

    Resolve absolute paths for script files on PATH (#134)

---
+++ CliWrap/Utils/PathResolver.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CliWrap.Utils;
+
+internal static class PathResolver
+{ 
+    internal static string ResolvePath(string fileName)
+    {
+        // Update target path if necessary
+        // We should use 'Path.IsPathFullyQualified', but its not available in .NET Standard 2.0
+        // https://github.com/dotnet/runtime/issues/22796
+        if (Path.IsPathRooted(fileName)) return fileName;
+        
+        if (File.Exists(fileName))
+            return Path.GetFullPath(fileName);
+
+        var envValues = Environment.GetEnvironmentVariable("PATH");
+        if (envValues == null) return fileName;
+        foreach (var path in envValues.Split(Path.PathSeparator))
+        {
+            var fullPath = Path.Combine(path, fileName);
+            
+            // we should look for .exe and .cmd files on windows
+            if (!fileName.EndsWith(".exe") && !fileName.EndsWith(".cmd") &&
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (File.Exists(fullPath + ".exe"))
+                    return fullPath + ".exe";
+                if (File.Exists(fullPath + ".cmd"))
+                    return fullPath + ".cmd";
+            }
+            else if (File.Exists(fullPath))
+            {
+                return fullPath;
+            }
+        }
+        // If we can't find the file, return the original path
+        return fileName;
+    }
+}
+
+ 
