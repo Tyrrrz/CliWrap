04d5d369499188a3861f0c424dd6eb084318449c
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-03-14 14:25:50 -0700

    Update CliWrap/Utils/NativeMethods.cs

--- CliWrap/Utils/NativeMethods.cs
+++ CliWrap/Utils/NativeMethods.cs
@@ -5,10 +5,6 @@ namespace CliWrap.Utils
 {
     internal static class NativeMethods
     {
-        /// <summary>
-        /// Returns a pointer to the console window.
-        /// </summary>
-        /// <returns>Pointer to the console window, IntPtr.Zero if there is no console.</returns>
         [DllImport("kernel32.dll")]
         public static extern IntPtr GetConsoleWindow();
     }
