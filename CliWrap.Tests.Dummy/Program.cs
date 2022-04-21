320696efa2989fd338695da6aecbd4919c00c616
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-04-21 20:01:26 +0000

    Update workflow

--- CliWrap.Tests.Dummy/Program.cs
+++ CliWrap.Tests.Dummy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CliFx;
 
 namespace CliWrap.Tests.Dummy;
@@ -7,9 +8,17 @@ public static class Program
 {
     public static string FilePath { get; } = typeof(Program).Assembly.Location;
 
-    public static async Task<int> Main(string[] args) =>
-        await new CliApplicationBuilder()
+    public static async Task<int> Main(string[] args)
+    {
+        // Make sure color codes are not produced because we rely on the output in tests
+        Environment.SetEnvironmentVariable(
+            "DOTNET_SYSTEM_CONSOLE_ALLOW_ANSI_COLOR_REDIRECTION",
+            "false"
+        );
+
+        return await new CliApplicationBuilder()
             .AddCommandsFromThisAssembly()
             .Build()
             .RunAsync(args);
+    }
 }
