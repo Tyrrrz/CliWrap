21d822050800e2321250c6a7fcfded04ef3a5afd
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2022-12-11 00:48:33 +0200

    asd

---
+++ CliWrap.Signaler/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CliWrap.Signaler;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        if (args is [var processIdRaw, var signalRaw])
+        {
+            var processId = int.Parse(processIdRaw);
+            var signal = int.Parse(signalRaw);
+
+            return 0;
+        }
+    }
+}
---
+++ CliWrap.Signaler/Utils/NativeMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CliWrap.Signaler.Utils;
+
+internal static class NativeMethods
+{
+    public static class Windows
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool FreeConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool AttachConsole(uint dwProcessId);
+    }
+}
