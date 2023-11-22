62e25bb854e7b35f2a22448d81dfb67be032d93c
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-22 22:38:50 +0200

    asd

--- CliWrap/CommandResult.cs
+++ CliWrap/CommandResult.cs
@@ -5,7 +5,7 @@ namespace CliWrap;
 /// <summary>
 /// Result of a command execution.
 /// </summary>
-public class CommandResult
+public partial class CommandResult
 {
     /// <summary>
     /// Exit code set by the underlying process.
@@ -41,9 +41,18 @@ public class CommandResult
         StartTime = startTime;
         ExitTime = exitTime;
     }
+}
+
+public partial class CommandResult
+{
+    /// <summary>
+    /// Converts the result to an integer value that corresponds to the <see cref="ExitCode" /> property.
+    /// </summary>
+    // This is only needed by CliWrap.Magic, but we can't implement this operator in another assembly
+    public static implicit operator int(CommandResult result) => result.ExitCode;
 
     /// <summary>
-    /// Converts the result to a boolean value indicating whether the command execution was successful.
+    /// Converts the result to a boolean value that corresponds to the <see cref="IsSuccess" /> property.
     /// </summary>
     // This is only needed by CliWrap.Magic, but we can't implement this operator in another assembly
     public static implicit operator bool(CommandResult result) => result.IsSuccess;
