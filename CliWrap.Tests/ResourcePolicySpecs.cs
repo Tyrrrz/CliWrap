6ce41db6b875e07262c3bd2685a8bfd28623d511
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-02-09 06:42:52 +0200

    Fix tests

--- CliWrap.Tests/ResourcePolicySpecs.cs
+++ CliWrap.Tests/ResourcePolicySpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
@@ -10,6 +11,11 @@ public class ResourcePolicySpecs
     [SkippableFact(Timeout = 15000)]
     public async Task I_can_execute_a_command_with_custom_process_priority()
     {
+        Skip.IfNot(
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
+            "Starting a process with custom priority is only supported on Windows."
+        );
+
         // Arrange
         var cmd = Cli.Wrap(Dummy.Program.FilePath)
             .WithResourcePolicy(p => p.SetPriority(ProcessPriorityClass.High));
@@ -24,6 +30,11 @@ public class ResourcePolicySpecs
     [SkippableFact(Timeout = 15000)]
     public async Task I_can_execute_a_command_with_custom_affinity_mask()
     {
+        Skip.IfNot(
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux),
+            "Starting a process with custom affinity is only supported on Windows and Linux."
+        );
+
         // Arrange
         var cmd = Cli.Wrap(Dummy.Program.FilePath).WithResourcePolicy(p => p.SetAffinity(0b1010)); // Cores 1 and 3
 
@@ -37,6 +48,11 @@ public class ResourcePolicySpecs
     [SkippableFact(Timeout = 15000)]
     public async Task I_can_execute_a_command_with_custom_working_set_limits()
     {
+        Skip.IfNot(
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
+            "Starting a process with custom priority is only supported on Windows."
+        );
+
         // Arrange
         var cmd = Cli.Wrap(Dummy.Program.FilePath)
             .WithResourcePolicy(p =>
