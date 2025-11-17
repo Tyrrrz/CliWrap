efa155f65d75aeb88c61501a97b1e3dd24ed778c
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-18 01:14:53 +0200

    Fix process init in signaler

--- CliWrap/Utils/WindowsSignaler.cs
+++ CliWrap/Utils/WindowsSignaler.cs
@@ -11,24 +11,23 @@ internal partial class WindowsSignaler(string filePath) : IDisposable
 {
     public bool TrySend(int processId, int signalId)
     {
-        using var process = new Process
+        using var process = new Process();
+
+        process.StartInfo = new ProcessStartInfo
         {
-            StartInfo = new ProcessStartInfo
+            FileName = filePath,
+            Arguments =
+                processId.ToString(CultureInfo.InvariantCulture)
+                + ' '
+                + signalId.ToString(CultureInfo.InvariantCulture),
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            Environment =
             {
-                FileName = filePath,
-                Arguments =
-                    processId.ToString(CultureInfo.InvariantCulture)
-                    + ' '
-                    + signalId.ToString(CultureInfo.InvariantCulture),
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                Environment =
-                {
-                    // This is a .NET 3.5 executable, so we need to configure framework rollover
-                    // to allow it to also run against .NET 4.0 and higher.
-                    // https://gist.github.com/MichalStrehovsky/d6bc5e4d459c23d0cf3bd17af9a1bcf5
-                    ["COMPLUS_OnlyUseLatestCLR"] = "1",
-                },
+                // This is a .NET 3.5 executable, so we need to configure framework rollover
+                // to allow it to also run against .NET 4.0 and higher.
+                // https://gist.github.com/MichalStrehovsky/d6bc5e4d459c23d0cf3bd17af9a1bcf5
+                ["COMPLUS_OnlyUseLatestCLR"] = "1",
             },
         };
 
