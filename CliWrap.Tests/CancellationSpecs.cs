20ab0bb81c9646866083e48e378bafb7ae9a2160
Kevin Petit <kevin.petit@hacksis.dev>
2026-07-20 16:33:19 +0200

    Modifications following review

--- CliWrap.Tests/CancellationSpecs.cs
+++ CliWrap.Tests/CancellationSpecs.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Reactive.Linq;
 using System.Text;
@@ -188,7 +188,7 @@ public class CancellationSpecs
     public async Task I_can_execute_a_command_as_a_pull_based_event_stream_with_no_unobserved_exception()
     {
         // Arrange
-        var unobservedExceptions = new List<Exception>();
+        var unobservedExceptions = new ConcurrentBag<Exception>();
 
         void OnUnobservedException(object? sender, UnobservedTaskExceptionEventArgs e)
         {
@@ -201,14 +201,15 @@ public class CancellationSpecs
         var cmd = Cli.Wrap("dotnet").WithArguments(["--version"]);
 
         // Act
+        // Listening to a pull event stream followed by cancelling the listening, should no trigger any UnobservedTaskException
+        // Since the issue is a race condition, run the operation multiple times and concurently to maximize the chances of triggering it
         try
         {
-            // Repeat to maximize race condition triggering
             for (var i = 0; i < 50; i++)
             {
                 using var cts = new CancellationTokenSource();
 
-                var act = Task.Run(async () =>
+                await Task.Run(async () =>
                 {
                     try
                     {
@@ -219,8 +220,6 @@ public class CancellationSpecs
                     }
                     catch (OperationCanceledException) { }
                 });
-
-                await act;
             }
 
             GC.Collect();
@@ -234,7 +233,7 @@ public class CancellationSpecs
         }
 
         // Assert
-        Assert.Empty(unobservedExceptions);
+        unobservedExceptions.Should().BeEmpty();
     }
 
     [Fact(Timeout = 15000)]
